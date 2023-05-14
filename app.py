998501911c43cb90ba3daf663bf80da31b82ead5
juansaracho <saracho007@outlook.com>
2023-05-14 09:07:41 -0700

    inititalized files

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+import numpy as np
+import pickle
+import joblib
+app = Flask(__name__)
+filename = 'file_cancer.pkl'
+#model = pickle.load(open(filename, 'rb'))
+model = joblib.load(filename)
+#model = joblib.load(filename)
+@app.route('/')
+def index(): 
+    return render_template('index.html')
+@app.route('/predict', methods=['POST'])
+def predict():
+    uniformity_cell_size = request.form['uniformity_cell_size']
+    uniformity_cell_shape = request.form['uniformity_cell_shape']
+    bare_nuclei = request.form['bare_nuclei']
+    bland_chromatin = request.form['bland_chromatin']
+
+    
+      
+    pred = model.predict(np.array([[uniformity_cell_size, uniformity_cell_shape, bare_nuclei, bland_chromatin ]]))
+    print(pred)
+    return render_template('index.html', predict=str(pred))
+
+
+if __name__ == '__main__':
+    app.run
